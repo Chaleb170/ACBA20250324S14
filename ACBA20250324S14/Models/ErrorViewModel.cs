66518b27b125bebb9b2faca06eaec62ce65fc9ff
Anderson Chaleb <Anderson Chaleb@DESKTOP-QV270QB>
2025-03-24 15:08:08 -0500

    Agregar archivos de proyecto.

---
+++ ACBA20250324S14/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ACBA20250324S14.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
